3c7c8f6f498a751fb85c4288bab16b52ea5b8dc5
1994bilal <bilalfiaz802@gmail.com>
2024-07-30 13:43:37 +0500

    update

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -112,10 +112,10 @@ export const CUSINES = [
 ];
 
 export const REVIEW = {
-  name: "Xaviour Fernando",
-  profession: "Food Critic",
+  name: "Bilal Fiaz",
+  profession: "CEO",
   content:
-    "“As a seasoned food critic, my expectations are always high when stepping into a new dining establishment. Restaura, with its unassuming exterior and elegantly designed interior, promised a unique culinary experience from the moment I walked in. And I must say, it delivered beyond my expectations.”",
+    "“As a seasoned food critic, my expectations are always high when stepping into a new dining establishment. Restaura, with its unassuming exterior and elegantly designed interior, promised aunique culinary experience from the moment I walked in. And I must say, it delivered beyond my expectations.”",
 };
 
 export const CONTACT = [
